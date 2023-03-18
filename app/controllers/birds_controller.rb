38a119b8850aa10f1790da4a16f7b354539b9060
Collins Chirchir <collinskchirchir@gmail.com>
2023-03-18 13:03:07 +0300

    creating custom routes

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -34,6 +34,16 @@ class BirdsController < ApplicationController
 
   end
 
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird, status: :accepted
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
